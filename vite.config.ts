22b6e07fa30007e7bb0a84967d27e014dbe43a0b
porchworrapon <worrapon12345@gmail.com>
2026-05-19 22:50:38 +0700

    add wrangler

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    middlewareMode: true,
   },
   plugins: [
     react(),
@@ -19,4 +20,9 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  preview: {
+    host: "::",
+    port: 8080,
+  },
+  appType: 'spa',
 }));
